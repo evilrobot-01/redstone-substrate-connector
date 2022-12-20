2bf1dfc12df28c3fdcd8508150a978282cb96f2b
Frank Bell <frank@parity.io>
2022-12-20 17:00:47 +0000

    docs: add comments to provide additional context

--- src/lib.rs
+++ src/lib.rs
@@ -10,23 +10,25 @@ use std::fmt::Debug;
 // then submit data on-chain where it is unpacked and verified to provide access to oracle prices.
 // This crate is intended to be used by Substrate pallets to easily unpack and verify that data.
 // The current implementation uses ECDSA signing, so the below has been designed to allow for this
-// base implementation, but also to allow for a more Substrate-optimised approach via config: e.g. a
-// 'SubstrateSigner' could be incorporated into the RedStone oracle itself, potentially providing
-// more efficient encoding of data using SCALE.
+// base implementation, but also to perhaps allow for a more Substrate-optimised approach via config:
+// e.g. a 'SubstrateSigner' could be incorporated into the RedStone oracle itself (using ECDSA or
+// SR25519), potentially providing more efficient encoding of data using the SCALE codec.
 
 // More info at https://github.com/redstone-finance/redstone-oracles-monorepo/tree/main/packages/evm-connector#data-packing-off-chain-data-encoding
 
-// NOTE: there is a reference implementation in the `near` directory which uses near_sdk::env::ecrecover
-// to determine the signer of a package.
+// NOTE: there is a reference implementation in the `near` directory attached which uses
+// near_sdk::env::ecrecover to determine the signer of a package.
 
+// Implementation options of the data package specifications in separate module.
 mod specifications;
 
 // The config used to decode signed off-chain data packages to on-chain oracle data.
+// This trait could allow a Substrate runtime to configure the data-types used.
 pub trait Config {
     // The type of data package specification.
     type DataPackageSpecification: DataPackageSpecification;
     // A converter for converting between a data point value and a result.
-    // todo: can this be improved?
+    // todo: improve
     type ValueConverter: TryConvert<
         <<Self as Config>::DataPackageSpecification as DataPackageSpecification>::Value,
         Self::Result,
@@ -39,7 +41,8 @@ pub trait Config {
     type MaxUnsignedMetadataLen: Get<u32>;
 }
 
-// A specification of a data package.
+// A specification of a data package, effectively lifting the structure of a data package into a type
+// and making it configurable. The specifications module contains two sample implementations.
 pub trait DataPackageSpecification {
     // The type of unique identifier of the data feed.
     type FeedId: Debug + PartialEq;
@@ -64,80 +67,17 @@ pub trait DataPackageSpecification {
     ) -> Option<&'a <Self::Signature as Pair>::Public>;
 }
 
-pub trait TryConvert<A, B> {
-    type Error;
-    fn try_convert(a: A) -> Result<B, Self::Error>;
-}
-
-#[derive(Decode, Encode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
-#[scale_info(skip_type_params(T))]
-pub struct Payload<
-    DataPackage,
-    MaxDataPackagesPerPayload: Get<u32>,
-    MaxUnsignedMetadataLen: Get<u32>,
-> {
-    data: BoundedVec<DataPackage, MaxDataPackagesPerPayload>,
-    unsigned_metadata: BoundedVec<u8, MaxUnsignedMetadataLen>,
-}
-
-impl<DataPackage, MaxDataPackagesPerPayload: Get<u32>, MaxUnsignedMetadataLen: Get<u32>>
-    Payload<DataPackage, MaxDataPackagesPerPayload, MaxUnsignedMetadataLen>
-{
-    pub fn new(
-        data: BoundedVec<DataPackage, MaxDataPackagesPerPayload>,
-        unsigned_metadata: BoundedVec<u8, MaxUnsignedMetadataLen>,
-    ) -> Payload<DataPackage, MaxDataPackagesPerPayload, MaxUnsignedMetadataLen> {
-        Payload {
-            data,
-            unsigned_metadata,
-        }
-    }
-}
-
-#[derive(Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
-#[scale_info(skip_type_params(DataPoint, MaxDataPointsPerPackage, Timestamp, Signature))]
-pub struct DataPackage<DataPoint, MaxDataPointsPerPackage: Get<u32>, Timestamp, Signature> {
-    data_points: BoundedVec<DataPoint, MaxDataPointsPerPackage>,
-    timestamp: Timestamp,
-    signature: Signature,
-}
-
-impl<DataPoint, MaxDataPointsPerPackage: Get<u32>, Timestamp, Signature>
-    DataPackage<DataPoint, MaxDataPointsPerPackage, Timestamp, Signature>
-{
-    pub fn new(
-        data_points: BoundedVec<DataPoint, MaxDataPointsPerPackage>,
-        timestamp: Timestamp,
-        signature: Signature,
-    ) -> DataPackage<DataPoint, MaxDataPointsPerPackage, Timestamp, Signature> {
-        DataPackage {
-            data_points,
-            timestamp,
-            signature,
-        }
-    }
-}
-
-#[derive(Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
-#[scale_info(skip_type_params(FeedId, Value))]
-pub struct DataPoint<FeedId, Value> {
-    feed_id: FeedId,
-    value: Value,
-}
-
-#[derive(Debug)]
-pub enum Error {
-    CannotTakeMedianOfEmptyArray,
-    ValueConversionError,
-}
-
-// Used internally within a pallet extrinsic to extract oracle values from submitted extrinsic parameter (of type `PayloadOf<T>`)
+// Main entry point: used internally within a pallet function (extrinsic/transaction) to extract
+// oracle values from submitted extrinsic parameter (of type `PayloadOf<T>`).
+// Note: This function has a generic type parameter which is used to inject the actual configuration,
+// much the same way as pallet config works. This *could* allow runtimes to use slightly different
+// implementations as required, provided there was sufficient benefit. A default config could be provided for most cases.
 pub fn get_oracle_value<T: Config>(
-    feed_id: &FeedIdOf<T>,
-    unique_signers_threshold: u8,
-    authorised_signers: &[SignerOf<T>],
-    current_timestamp_milliseconds: u128,
-    payload: &PayloadOf<T>,
+    feed_id: &FeedIdOf<T>, // A type alias, specifying the FeedId type defined in supplied config.
+    _unique_signers_threshold: u8,
+    authorised_signers: &[SignerOf<T>], // A type alias, specifying the Signer type defined in supplied config.
+    _current_timestamp_milliseconds: u128,
+    payload: &PayloadOf<T>, // A type alias, specifying the payload type defined in supplied config.
 ) -> Result<T::Result, Error> {
     // todo: complete implementation
     let mut values: Vec<T::Result> = vec![];
@@ -179,6 +119,87 @@ fn aggregate_values<T: Config>(values: &mut Vec<T::Result>) -> Result<T::Result,
     } else {
         values[mid]
     })
+}
+
+// Simple trait for converting from data package value type (e.g. bytes) to a rust primitive type (e.g. u128).
+pub trait TryConvert<A, B> {
+    type Error;
+    fn try_convert(a: A) -> Result<B, Self::Error>;
+}
+
+// An oracle payload, submitted on-chain as an extrinsic (transaction) parameter.
+#[derive(Decode, Encode, MaxEncodedLen, RuntimeDebug, TypeInfo)] // Derive macros which implement SCALE encoding of the type.
+#[scale_info(skip_type_params(DataPackage, MaxDataPackagesPerPayload, MaxUnsignedMetadataLen))] // Ignores generics on scale encoding
+pub struct Payload<
+    DataPackage, // Generic parameters, as the actual types are based on config.
+    MaxDataPackagesPerPayload: Get<u32>,
+    MaxUnsignedMetadataLen: Get<u32>,
+> {
+    // The data packages within the payload.
+    data: BoundedVec<DataPackage, MaxDataPackagesPerPayload>, // BoundedVec constrains vector capacity to some max size (defined in config)
+    // Any unsigned metadata.
+    unsigned_metadata: BoundedVec<u8, MaxUnsignedMetadataLen>,
+}
+
+impl<DataPackage, MaxDataPackagesPerPayload: Get<u32>, MaxUnsignedMetadataLen: Get<u32>>
+    Payload<DataPackage, MaxDataPackagesPerPayload, MaxUnsignedMetadataLen>
+{
+    // Simple helper function
+    pub fn new(
+        data: BoundedVec<DataPackage, MaxDataPackagesPerPayload>,
+        unsigned_metadata: BoundedVec<u8, MaxUnsignedMetadataLen>,
+    ) -> Payload<DataPackage, MaxDataPackagesPerPayload, MaxUnsignedMetadataLen> {
+        Payload {
+            data,
+            unsigned_metadata,
+        }
+    }
+}
+
+// A data package within a payload.
+#[derive(Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+#[scale_info(skip_type_params(DataPoint, MaxDataPointsPerPackage, Timestamp, Signature))]
+pub struct DataPackage<DataPoint, MaxDataPointsPerPackage: Get<u32>, Timestamp, Signature> {
+    // The data points within the payload.
+    data_points: BoundedVec<DataPoint, MaxDataPointsPerPackage>,
+    // The corresponding timestamp.
+    timestamp: Timestamp,
+    // A signature of the data package (data-points and timestamp).
+    signature: Signature,
+}
+
+impl<DataPoint, MaxDataPointsPerPackage: Get<u32>, Timestamp, Signature>
+    DataPackage<DataPoint, MaxDataPointsPerPackage, Timestamp, Signature>
+{
+    // Simple helper function
+    pub fn new(
+        data_points: BoundedVec<DataPoint, MaxDataPointsPerPackage>,
+        timestamp: Timestamp,
+        signature: Signature,
+    ) -> DataPackage<DataPoint, MaxDataPointsPerPackage, Timestamp, Signature> {
+        DataPackage {
+            data_points,
+            timestamp,
+            signature,
+        }
+    }
+}
+
+// A data point within a data package.
+#[derive(Encode, Decode, MaxEncodedLen, RuntimeDebug, TypeInfo)]
+#[scale_info(skip_type_params(FeedId, Value))]
+pub struct DataPoint<FeedId, Value> {
+    // The feed identifier.
+    feed_id: FeedId,
+    // The data point value.
+    value: Value,
+}
+
+// Errors potentially produced by the crate: runtime code must not panic!
+#[derive(Debug)]
+pub enum Error {
+    CannotTakeMedianOfEmptyArray,
+    ValueConversionError,
 }
 
 // Type helpers
--- src/specifications.rs
+++ src/specifications.rs
@@ -1,12 +1,15 @@
 use super::*;
 use std::marker::PhantomData;
 
+// An EVM-based implementation of a data package.
 pub struct EVM<MaxDataPointsPerPackage> {
     data: PhantomData<MaxDataPointsPerPackage>,
 }
+// Implementation of the data package specification trait based on existing EVM settings.
 impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
     for EVM<MaxDataPointsPerPackage>
 {
+    // Define the standard types as used by the EVMSigner/Connector
     type FeedId = [u8; 32];
     type Value = [u8; 32];
     type Timestamp = [u8; 6];
@@ -22,7 +25,8 @@ impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
         >,
         authorised_signers: &'a [<Self::Signature as Pair>::Public],
     ) -> Option<&'a <Self::Signature as Pair>::Public> {
-        // todo: optimise
+        // Serialise the data package (and hash) to recover the public key for comparison
+        // todo: optimise & refactor hashing into separate trait/type
         let message = Self::serialise(&data_package.data_points, &data_package.timestamp);
 
         // todo: optimise between signature.recover vs verifying until first trusted signer found
@@ -39,10 +43,30 @@ impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
         None
     }
 }
+// Implementation of serialise trait for serialising a data package
+impl<MaxDataPointsPerPackage: Debug + Get<u32>> Serialise for EVM<MaxDataPointsPerPackage> {
+    type DataPoint = DataPoint<
+        <Self as DataPackageSpecification>::FeedId,
+        <Self as DataPackageSpecification>::Value,
+    >;
+    type Timestamp = <Self as DataPackageSpecification>::Timestamp;
 
+    fn serialise(data_points: &[Self::DataPoint], timestamp: &Self::Timestamp) -> [u8; 32] {
+        // Data already byte arrays so simply add to a vector and then hash
+        // todo: optimise
+        let mut message = Vec::new();
+        message.extend(data_points.iter().flat_map(|dp| dp.feed_id));
+        message.extend(data_points.iter().flat_map(|dp| dp.value));
+        message.extend(timestamp);
+        sp_core::keccak_256(&message)
+    }
+}
+
+// A Substrate-based implementation of a data package, using data types more natural to Substrate.
 pub struct Substrate<MaxDataPointsPerPackage> {
     data: PhantomData<MaxDataPointsPerPackage>,
 }
+// Implementation of the data package specification trait using compact number encoding
 impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
     for Substrate<MaxDataPointsPerPackage>
 {
@@ -61,6 +85,7 @@ impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
         >,
         authorised_signers: &'a [<Self::Signature as Pair>::Public],
     ) -> Option<&'a <Self::Signature as Pair>::Public> {
+        // Serialise the data package (and hash) to generate the message for signature verification
         // todo: optimise
         let message = Self::serialise(&data_package.data_points, &data_package.timestamp);
         for signer in authorised_signers {
@@ -72,30 +97,7 @@ impl<MaxDataPointsPerPackage: Get<u32> + Debug> DataPackageSpecification
         None
     }
 }
-
-trait Serialise {
-    type DataPoint;
-    type Timestamp;
-    fn serialise(data_points: &[Self::DataPoint], timestamp: &Self::Timestamp) -> [u8; 32];
-}
-
-impl<MaxDataPointsPerPackage: Debug + Get<u32>> Serialise for EVM<MaxDataPointsPerPackage> {
-    type DataPoint = DataPoint<
-        <Self as DataPackageSpecification>::FeedId,
-        <Self as DataPackageSpecification>::Value,
-    >;
-    type Timestamp = <Self as DataPackageSpecification>::Timestamp;
-
-    fn serialise(data_points: &[Self::DataPoint], timestamp: &Self::Timestamp) -> [u8; 32] {
-        // todo: optimise
-        let mut message = Vec::new();
-        message.extend(data_points.iter().flat_map(|dp| dp.feed_id));
-        message.extend(data_points.iter().flat_map(|dp| dp.value));
-        message.extend(timestamp);
-        sp_core::keccak_256(&message)
-    }
-}
-
+// Implementation of serialise trait for serialising a data package
 impl<MaxDataPointsPerPackage: Debug + Get<u32>> Serialise for Substrate<MaxDataPointsPerPackage> {
     type DataPoint = DataPoint<
         <Self as DataPackageSpecification>::FeedId,
@@ -104,15 +106,26 @@ impl<MaxDataPointsPerPackage: Debug + Get<u32>> Serialise for Substrate<MaxDataP
     type Timestamp = <Self as DataPackageSpecification>::Timestamp;
 
     fn serialise(data_points: &[Self::DataPoint], timestamp: &Self::Timestamp) -> [u8; 32] {
+        // Some data (value/timestamp) needs converting to bytes before hashing, would be good to
+        // benchmark against EVM implementation for performance/size comparison
         // todo: optimise
         let mut message = Vec::new();
         message.extend(data_points.iter().flat_map(|dp| dp.feed_id));
         message.extend(data_points.iter().flat_map(|dp| dp.value.encode()));
         message.extend(timestamp.encode());
-        sp_core::blake2_256(&message)
+        sp_core::blake2_256(&message) // Use blake2 hashing for comparison
     }
 }
 
+// A simple trait to allow serialisation of a data package for signature verification.
+trait Serialise {
+    // Only use these two types to avoid complexity of using DataPackage<..>
+    type DataPoint;
+    type Timestamp;
+    fn serialise(data_points: &[Self::DataPoint], timestamp: &Self::Timestamp) -> [u8; 32];
+}
+
+// Various tests which demonstrate usage.
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -131,15 +144,17 @@ mod tests {
         "0000000000000000000000000000000000000000000000000000002e90edd000";
     const TIMESTAMP_HEX_STR: &str = "01812f2590c0"; // 1654353400000 in hex
 
+    // Simple types to constrain maximum vector values
     type MaxDataPointsPerPackage = ConstU32<10>;
     type MaxDataPackagesPerPayload = ConstU32<10>;
     type MaxUnsignedMetadataLen = ConstU32<100>;
 
+    // Implementation of config using the EVM-based data spec.
     #[derive(Debug)]
     struct EVMConfig;
     impl Config for EVMConfig {
-        type DataPackageSpecification = EVM<MaxDataPointsPerPackage>;
-        type ValueConverter = Self;
+        type DataPackageSpecification = EVM<MaxDataPointsPerPackage>; // EVM data spec
+        type ValueConverter = Self; // Converter of byte array to oracle result
         type Result = u128;
         type MaxDataPackagesPerPayload = MaxDataPackagesPerPayload;
         type MaxUnsignedMetadataLen = MaxUnsignedMetadataLen;
@@ -147,95 +162,12 @@ mod tests {
     impl TryConvert<ValueOf<EVMConfig>, u128> for EVMConfig {
         type Error = TryFromSliceError;
         fn try_convert(value: ValueOf<EVMConfig>) -> Result<u128, Self::Error> {
+            // Convert byte array to u128
             Ok(u128::from_be_bytes(value[16..].try_into()?))
         }
     }
 
-    #[test]
-    fn data_point_value_conversion() {
-        const VALUE: u128 = 42000 * 10u128.pow(8);
-        let mut value = vec![0; 16];
-        value.append(VALUE.to_be_bytes().to_vec().as_mut());
-        let value: ValueOf<EVMConfig> = value.try_into().unwrap();
-        let value_hex = to_hex(&value, false);
-        assert_eq!(
-            "0x000000000000000000000000000000000000000000000000000003d1e3821000",
-            value_hex
-        );
-
-        let value: [u8; 16] = from_hex(&value_hex).unwrap()[16..].try_into().unwrap();
-        assert_eq!(VALUE, u128::from_be_bytes(value))
-    }
-
-    #[test]
-    fn timestamp_conversion() {
-        const TIMESTAMP: u64 = 1654353400000;
-
-        // Timestamp is 6 bytes in RedStone data package specification
-        let timestamp: TimestampOf<EVMConfig> = TIMESTAMP.to_be_bytes()[2..].try_into().unwrap();
-        let timestamp_hex = to_hex(&timestamp, false);
-        assert_eq!("0x01812f2590c0", timestamp_hex);
-
-        let mut timestamp = vec![0; 2];
-        timestamp.append(from_hex(&timestamp_hex).unwrap().as_mut());
-        let timestamp: [u8; 8] = timestamp.try_into().unwrap();
-        assert_eq!(TIMESTAMP, u64::from_be_bytes(timestamp))
-    }
-
-    #[test]
-    fn initial() {
-        let timestamp = from_hex(TIMESTAMP_HEX_STR).unwrap().try_into().unwrap();
-
-        let unsigned_metadata: BoundedUnsignedMetadataOf<EVMConfig> = "1.1.2#test-data-feed"
-            .as_bytes()
-            .to_vec()
-            .try_into()
-            .unwrap();
-
-        let package = DataPackage::new(
-            bounded_vec![
-                DataPoint {
-                    feed_id: from_hex(BTC_BYTES_32_HEX_STR).unwrap().try_into().unwrap(),
-                    value: from_hex(BTC_VALUE_HEX_STR).unwrap().try_into().unwrap(),
-                },
-                DataPoint {
-                    feed_id: from_hex(ETH_BYTES_32_HEX_STR).unwrap().try_into().unwrap(),
-                    value: from_hex(ETH_VALUE_HEX_STR).unwrap().try_into().unwrap(),
-                }
-            ],
-            timestamp,
-            Default::default(),
-        );
-        let payload = PayloadOf::<EVMConfig> {
-            data: bounded_vec![package],
-            unsigned_metadata,
-        };
-
-        let encoded = payload.encode();
-        println!("{} {:?}", encoded.len(), encoded);
-
-        let decoded = PayloadOf::<EVMConfig>::decode(&mut &encoded[..]).unwrap();
-        println!("{:?}", decoded);
-
-        assert_eq!(payload.unsigned_metadata, decoded.unsigned_metadata)
-    }
-
-    #[test]
-    fn from_hex_equals_decode_hex() {
-        // Copied from redstone-near-connectors/rust/tests/integration_test.rs
-        fn decode_hex(s: &str) -> Result<Vec<u8>, core::num::ParseIntError> {
-            (0..s.len())
-                .step_by(2)
-                .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-                .collect()
-        }
-
-        assert_eq!(
-            from_hex(BTC_BYTES_32_HEX_STR).unwrap(),
-            decode_hex(BTC_BYTES_32_HEX_STR).unwrap()
-        )
-    }
-
+    // Gets oracle value using EVM data spec
     #[test]
     fn gets_oracle_value() {
         let btc = from_hex(BTC_BYTES_32_HEX_STR).unwrap().try_into().unwrap();
@@ -282,10 +214,11 @@ mod tests {
         assert_eq!(42_000 * 100_000_000, value);
     }
 
+    // Implementation of config using the Substrate-based data spec.
     #[derive(Debug)]
     struct SubstrateConfig;
     impl Config for SubstrateConfig {
-        type DataPackageSpecification = Substrate<MaxDataPointsPerPackage>;
+        type DataPackageSpecification = Substrate<MaxDataPointsPerPackage>; // Substrate data spec
         type ValueConverter = Self;
         type Result = u128;
         type MaxDataPackagesPerPayload = MaxDataPackagesPerPayload;
@@ -294,16 +227,19 @@ mod tests {
     impl TryConvert<ValueOf<SubstrateConfig>, u128> for SubstrateConfig {
         type Error = TryFromSliceError;
         fn try_convert(value: ValueOf<SubstrateConfig>) -> Result<u128, Self::Error> {
+            // Value already deserialised based on config, so simply return value
             Ok(value.0)
         }
     }
 
+    // Create EVM/Substrate-based payloads for comparison
     #[test]
     fn compare_data_package_encodings() {
         let btc = from_hex(BTC_BYTES_32_HEX_STR).unwrap().try_into().unwrap();
         let eth = from_hex(ETH_BYTES_32_HEX_STR).unwrap().try_into().unwrap();
         let unsigned_metadata = BoundedVec::default();
 
+        // Create 'EVM' payload
         let ecdsa_signer = ecdsa::Pair::from_seed(b"12345678901234567890123456789012");
         let data_points: BoundedDataPointsOf<EVMConfig> = bounded_vec![
             DataPoint {
@@ -326,6 +262,7 @@ mod tests {
             unsigned_metadata.clone(),
         );
 
+        // Create 'Substrate' payload
         let sr25519_signer = sr25519::Pair::from_seed(b"12345678901234567890123456789012");
         let data_points: BoundedDataPointsOf<SubstrateConfig> = bounded_vec![
             DataPoint {
@@ -383,5 +320,94 @@ mod tests {
             substrate.len(),
             substrate
         );
+    }
+
+    // Just checking payload encoding
+    #[test]
+    fn payload_encoding() {
+        let timestamp = from_hex(TIMESTAMP_HEX_STR).unwrap().try_into().unwrap();
+
+        let unsigned_metadata: BoundedUnsignedMetadataOf<EVMConfig> = "1.1.2#test-data-feed"
+            .as_bytes()
+            .to_vec()
+            .try_into()
+            .unwrap();
+
+        let package = DataPackage::new(
+            bounded_vec![
+                DataPoint {
+                    feed_id: from_hex(BTC_BYTES_32_HEX_STR).unwrap().try_into().unwrap(),
+                    value: from_hex(BTC_VALUE_HEX_STR).unwrap().try_into().unwrap(),
+                },
+                DataPoint {
+                    feed_id: from_hex(ETH_BYTES_32_HEX_STR).unwrap().try_into().unwrap(),
+                    value: from_hex(ETH_VALUE_HEX_STR).unwrap().try_into().unwrap(),
+                }
+            ],
+            timestamp,
+            Default::default(),
+        );
+        let payload = PayloadOf::<EVMConfig> {
+            data: bounded_vec![package],
+            unsigned_metadata,
+        };
+
+        let encoded = payload.encode();
+        println!("{} {:?}", encoded.len(), encoded);
+
+        let decoded = PayloadOf::<EVMConfig>::decode(&mut &encoded[..]).unwrap();
+        println!("{:?}", decoded);
+
+        assert_eq!(payload.unsigned_metadata, decoded.unsigned_metadata)
+    }
+
+    // Just checking conversions work as expected
+    #[test]
+    fn data_point_value_conversion() {
+        const VALUE: u128 = 42000 * 10u128.pow(8);
+        let mut value = vec![0; 16];
+        value.append(VALUE.to_be_bytes().to_vec().as_mut());
+        let value: ValueOf<EVMConfig> = value.try_into().unwrap();
+        let value_hex = to_hex(&value, false);
+        assert_eq!(
+            "0x000000000000000000000000000000000000000000000000000003d1e3821000",
+            value_hex
+        );
+
+        let value: [u8; 16] = from_hex(&value_hex).unwrap()[16..].try_into().unwrap();
+        assert_eq!(VALUE, u128::from_be_bytes(value))
+    }
+
+    // Just checking conversions work as expected
+    #[test]
+    fn timestamp_conversion() {
+        const TIMESTAMP: u64 = 1654353400000;
+
+        // Timestamp is 6 bytes in RedStone data package specification
+        let timestamp: TimestampOf<EVMConfig> = TIMESTAMP.to_be_bytes()[2..].try_into().unwrap();
+        let timestamp_hex = to_hex(&timestamp, false);
+        assert_eq!("0x01812f2590c0", timestamp_hex);
+
+        let mut timestamp = vec![0; 2];
+        timestamp.append(from_hex(&timestamp_hex).unwrap().as_mut());
+        let timestamp: [u8; 8] = timestamp.try_into().unwrap();
+        assert_eq!(TIMESTAMP, u64::from_be_bytes(timestamp))
+    }
+
+    // Just checking conversions work as expected
+    #[test]
+    fn from_hex_equals_decode_hex() {
+        // Copied from redstone-near-connectors/rust/tests/integration_test.rs
+        fn decode_hex(s: &str) -> Result<Vec<u8>, core::num::ParseIntError> {
+            (0..s.len())
+                .step_by(2)
+                .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+                .collect()
+        }
+
+        assert_eq!(
+            from_hex(BTC_BYTES_32_HEX_STR).unwrap(),
+            decode_hex(BTC_BYTES_32_HEX_STR).unwrap()
+        )
     }
 }
